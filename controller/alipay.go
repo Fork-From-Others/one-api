b76d9281af24669553aa89d9947c7ee7c7cf878c
刘德财 <442823529@qq.com>
2024-01-01 12:30:59 +0800

    临时存储

--- controller/alipay.go
+++ controller/alipay.go
@@ -1,11 +1,15 @@
 package controller
 
 import (
+	"context"
 	"github.com/gin-gonic/gin"
 	"github.com/go-pay/gopay"
 	"github.com/go-pay/gopay/alipay"
 	"github.com/go-pay/gopay/pkg/xlog"
+	"github.com/go-pay/gopay/pkg/xpem"
+	"github.com/go-pay/gopay/pkg/xrsa"
 	"net/http"
+	"time"
 )
 
 const (
@@ -14,6 +18,8 @@ const (
 	PrivateKey           = "MIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQCZEdwKkPYQOzfDCSMBksPfjGeOn0kWWwql0nGVa7QkGcCnzUfBD4Z4Zq3Krhu5w3TFSOHVyH/JHQ3R1KxZGgVWsfD3hCkQNyQCYgP1qTAHvrFsQwbIJyTwfJYgqFftR+USpbXuGy026yexX3EyvgJ+XBc/Th0r3K72fFImg4Hxz/VzoU7c2xb9MZm1d+4W1H6yn7vPjK+/Gsvjrvo6GsWGira9Thg+Y7ScbNY2wvJCi4hgSSAYsSUu5YdlhTSvenOp1U8KnmDxCunxV751pEfwidkH9HStUvuxClO/c+9J5xS0w+fbWfvux8SE3gZvObEdMc5NLLwKJQ2Tbo6AfW17AgMBAAECggEAGiSDuUZC0Ejc4DGaSfWAAJkhQqmPuQK5kdKcVZG8hYHkdoRH4gA9zihzPC96SsLIGb213GZO9NFCf/jbqqgYC1N+vTdUMBHK06Fb3cQUkO4PrVbRPLP6yhvtJAy8X6ksxX+Jz+3SThPhhpivY6QyFtSxn70+nDQnqa9X9H33Xo4LAE+EzivbOmZeRC1JXsFzZSbLIvMwibXg+dTf0bWl17rmePDJtxLW7c+20iVFMynJETI7uIBN2gHkp7vLAOFVfsMSK0rYNViNKA4hUhJw1SedbO3BwPdsxOZouYm0MY+w3Y5N1OBEZQ8jjMOVBlTfd0GCodPtYYchehbC0DwvmQKBgQDautxdM2QGPQiQ0kb1QMDbKABn+O86rm+TDKJh6x+dQps/hgLnAcvX57xNVmIQca+/WSAcXffnmV2czTugmRnfdy0Y89g9tUPurFyu09SfRV5Vrj0bQY6jdXFArZkkAsNec2bdWOso8Xnl7wtH00TD75x2ureNi714xz8J2qdA7wKBgQCzJtxLBxvGwXNIdSToAHwFR73yHVN62pdamtrhZGx+2RX/2oVuTn0l8rmff+G7W+rOeMdVIolW9trPaMXvvaDcpwOVkaOXHmSZmDQMT6O5mk2xT0KBoqGnnLS8EWkJpGlv64XnSIsEujXFnKLJugj3a+iYv3qTEmq26rDQCDrENQKBgCkazKbHLZjuh1mP6r3UOWn2Dn17jpmchmNAEJQON5a6GarKaGk5MTGV3xE5lpw4gSqYeSxbjGb9r1X0S6xWmUIhh1wVFyIhmm6T/abtMBvuUVgQsnMY0tFtFKdu+ESIMGbjkQUv3KGJH7tSPPB2h4m60dCOLkhvZl/4MaSMbroJAoGASPnjcoyKvAPBOhq91eOcoWn/7cgUYU75qGa8EmQd7e3wEDCreatvPy4IfvhQs0lV9JUuXXecCliz+RjsyCOuizNdOmgBA2XWBNsDGKC4SLqaO0fWB4h/4Q7scE+HQe4/JOADw5rBRkOz87NCfHnTfTXvoYkeHRq7bZdcPuGbTqECgYB/EvNv9/0piSv5VTkLZHUBsSSnaYzIbI9FjUSHSgMYdUB2Q83JKkvJh549p9p1OzSLxVcvu8G3BllWgvOhEzrl97Nhi9I/8tfDbD2oXlrGJyumcslMtsasSuWjiLQUoLOc8lkTEtIW4MNCCvDsUg7SshAZFq+RDlTgrIWiAZKcQQ=="
 	isPro                = false
 )
+
+var ctx = context.Background()
 
 func GetPaymentQrcode(c *gin.Context) {
 	client, err := alipay.NewClient(AppId, PrivateKey, isPro)
@@ -33,13 +39,41 @@ func GetPaymentQrcode(c *gin.Context) {
 	//请求参数
 	bm := make(gopay.BodyMap)
 	bm.Set("app_id", AppId)
+	bm.Set("timestamp", time.Now().Format("2006-01-02 15:04:05"))
+	bm.Set("charset", "utf-8")
+	bm.Set("version", "1.0")
+	bm.Set("notify_url", "")
+	bm.Set("method", "alipay.trade.precreate")
+	bm.Set("biz_content", "trade_no_123")
 	bm.Set("subject", "测试商品")
 	bm.Set("out_trade_no", "trade_no_123")
 	bm.Set("total_amount", "1.00")
 	bm.Set("product_code", "FACE_TO_FACE_PAYMENT")
 
+	key := xrsa.FormatAlipayPrivateKey(PrivateKey)
+	priKey, err := xpem.DecodePrivateKey([]byte(key))
+	if err != nil {
+		xlog.Error(err)
+		c.JSON(http.StatusOK, gin.H{
+			"success": false,
+			"message": err.Error(),
+		})
+		return
+	}
+	sign, err := alipay.GetRsaSign(bm, alipay.RSA2, priKey)
+	if err != nil {
+		xlog.Error(err)
+		c.JSON(http.StatusOK, gin.H{
+			"success": false,
+			"message": err.Error(),
+		})
+		return
+	}
+	bm.Set("sign", sign)
+	bm.Set("sign_type", alipay.RSA2)
+
 	//创建订单
-	aliRsp, err := client.TradePrecreate(c, bm)
+	aliRsp, err := client.TradePrecreate(ctx, bm)
 	if err != nil {
 		xlog.Error("err:", err)
 		c.JSON(http.StatusOK, gin.H{
